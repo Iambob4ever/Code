ff3831f179b164acf4d8c3695655ecacafae5b7d
iambob4ever <bob.boettcher@hotmail.com>
2021-05-20 22:14:32 -0400

    inital commit

---
+++ Control_loops.py
@@ -0,0 +1,10 @@
+#Comparisons
+a = 2
+b = 5
+
+print(a == b) # a is equal to b
+print(a != b) # a not equal to b
+print(a < b)  # a is smaller than b
+print(a > b)
+print(a <= b) # a is smaller than or equal to b
+print(a >= b) # 
